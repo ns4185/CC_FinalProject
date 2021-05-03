a8df436b0c932480e492da4d5e01fc6741482540
ns4185 <ns4185@nyu.edu>
2021-05-03 15:36:34 -0400

    main scenes/preload
    main scenes are now in the images folder - the main .js file includes the preload function

---
+++ finalproject.js
@@ -0,0 +1,38 @@
+let home;
+let lobby;
+let closetWindow;
+let gravestone;
+let upstairsHallway;
+let upOrDownstairs;
+let basement;
+let bedroom;
+// let fade = 0; //This is a variable so that images fade to black/fade from black when they're called
+
+function preload(){
+    home = loadImage('images/homescreen.png');
+    lobby = loadImage('images/lobby.png');
+    closetWindow = loadImage('images/closetwindow.png');
+    gravestone = loadImage('images/gravestone.png');
+    upstairsHallway = loadImage('images/upstairs.png');
+    upOrDownstairs = loadImage('images/upordownstairs.png');
+    basement = loadImage('images/basement.png');
+    bedroom = loadImage('images/bedroom.png');
+}
+
+function setup(){
+    createCanvas(1280,720);
+    background(0);
+}
+
+function draw(){
+    // tint(255,50); //This tint is here to apply transparency on the images without affecting the color
+    // image(home, 0, 0);
+    // image(lobby, 0, 0);
+    // image(closetWindow, 0, 0);
+    // image(gravestone, 0, 0);
+    // image(upstairsHallway , 0, 0);
+    // image(upOrDownstairs, 0, 0);
+    // image(basement, 0, 0);
+    // image(bedroom, 0, 0);
+
+}

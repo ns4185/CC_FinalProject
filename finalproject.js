272d535efd98de4bcc28e88d9c890cbe35cfe37b
ns4185 <ns4185@nyu.edu>
2021-05-03 20:49:24 -0400

    beta update part 1 (framework 1 of 2)
    framework for the entire "go towards sound" path

--- finalproject.js
+++ finalproject.js
@@ -1,3 +1,4 @@
+//IMAGES
 let home;
 let lobby;
 let closetWindow;
@@ -6,9 +7,30 @@ let upstairsHallway;
 let upOrDownstairs;
 let basement;
 let bedroom;
-// let fade = 0; //This is a variable so that images fade to black/fade from black when they're called
+
+//FONTS
+let horror;
+let assassin;
+
+//SOUNDS
+let spooky;
+
+
+//BOOLEANS
+let onhomescreen;
+let onhomescreencounter=0;
+let onlobbyscene=false;
+let onstairscene=false;
+let onbasementscene1=false;
+let onbasementscene2=false;
+let onbasementscene3=false;
+let onbasementscene5=false;
+let onupstairsscene=false;
+let onroomscene=false;
+let oncoffinscene1=false;
 
 function preload(){
+    //IMAGES
     home = loadImage('images/homescreen.png');
     lobby = loadImage('images/lobby.png');
     closetWindow = loadImage('images/closetwindow.png');
@@ -17,22 +39,89 @@ function preload(){
     upOrDownstairs = loadImage('images/upordownstairs.png');
     basement = loadImage('images/basement.png');
     bedroom = loadImage('images/bedroom.png');
+
+    //FONTS
+    montserrat = loadFont('Fonts/Montserrat.ttf');
+    horror = loadFont('Fonts/Horror.ttf');
+
+    //SOUNDS
+    soundFormats('mp3');
+    spooky = loadSound('Sounds/spooky.mp3');
 }
 
 function setup(){
     createCanvas(1280,720);
     background(0);
+    homescreen();
 }
 
 function draw(){
-    // tint(255,50); //This tint is here to apply transparency on the images without affecting the color
-    // image(home, 0, 0);
-    // image(lobby, 0, 0);
-    // image(closetWindow, 0, 0);
-    // image(gravestone, 0, 0);
-    // image(upstairsHallway , 0, 0);
-    // image(upOrDownstairs, 0, 0);
-    // image(basement, 0, 0);
-    // image(bedroom, 0, 0);
+    
+}
+
+function mousePressed(){
+    if (onhomescreen==true && onhomescreencounter==0){
+        spooky.setVolume(0.25);
+        spooky.play(); 
+    }
+}
+
+function keyPressed(){
+    if (key === '1' && onhomescreen == true){ 
+        onhomescreen=false;
+        onhomescreencounter=1;
+        spooky.stop();
+        lobbyscene();
+    } else if (key === '1' && onlobbyscene == true){ //FOLLOW THE NOISE
+        onlobbyscene=false;
+        stairwell();
+    } else if (key === '1' && onstairscene == true){ //GO DOWN TO THE BASEMENT
+        onstairscene=false;
+        basementscene1();
+    } else if (key === '1' && onbasementscene1 == true){ //TURN THE LIGHTS ON
+        onbasementscene1=false;
+        basementscene2();
+    } else if (key === '1' && onbasementscene2 == true){ //SPIDERS-DO NOTHING
+        onbasementscene2=false;
+        basementscene3();
+    } else if (key === '1' && onbasementscene5 == true){ //DOG-RUN AWAY
+        onbasementscene5=false;
+        basementscene6();
+    } else if (key === '1' && onupstairsscene== true){ //ENTER THE ROOM
+        onupstairsscene=false;
+        roomscene();
+    } else if (key === '1' && onroomscene== true){ //SAY BOO
+        onupstairsscene=false;
+        boo();
+    } else if (key === '1' && oncoffinscene1== true){ //SAY BOO
+        oncoffinscene1=false;
+        coffinscene2();
+    }
+
+
+    
+
+    if (key === '2' && onbasementscene2 ==true){ //SPIDERS-SCREAM
+        onbasementscene2=false;
+        basementscene4();
+    } else if (key === '2' && onbasementscene1 == true){ //KEEP THE LIGHTS OFF
+        onbasementscene1=false;
+        basementscene5();
+    } else if (key === '2' && onbasementscene5 == true){ //DOG-POKE
+        onbasementscene5=false;
+        basementscene7();
+    } else if (key === '2' && onstairscene == true){ //GO UPSTAIRS
+        onstairscene=false;
+        upstairsscene();
+    } else if (key === '2' && onroomscene== true){ //THROW SOMETHING
+        onupstairsscene=false;
+        thrown();
+    } else if (key === '2' && onupstairsscene== true){ //APPROACH THE COFFIN
+        onupstairsscene=false;
+        coffinscene1();
+    } else if (key === '2' && oncoffinscene1== true){ //SAY BOO
+        oncoffinscene1=false;
+        coffinscene3();
+    }
 
 }
--- scenes.js
+++ scenes.js
@@ -1,6 +1,202 @@
 function homescreen(){
+    image(home, 0, 0);
+    onhomescreen=true;
+
+    fill(255);
+    textFont(horror);
+    textSize(100);
+    text('House of',30,300);
+    text('Horrors',80,400);
 
 
+    textFont(montserrat);
+    textSize(10);
+    text('A SHORT INTERACTIVE NARRATIVE',80,450);
 
-    
+    textSize(8);
+    text('[CLICK TO ENABLE SOUND]'); //for some reason the audio wouldn't play when I put the play command into setup, so I made it so that the user manually starts the music
+
+    textFont(montserrat);
+    textSize(20);
+    text('PRESS 1 TO START',1000,350);
+
+
+}
+
+function lobbyscene(){ //entered the house
+    image(lobby, 0, 0);
+    onlobbyscene = true;
+
+    textFont(montserrat);
+    textSize(20);
+    text('FOLLOW THE SOUND [1]',80,350);
+}
+
+function stairwell(){ //decide to follow the noise
+    image(upOrDownstairs,0,0);
+    onstairscene=true;
+
+    textFont(montserrat);
+    textSize(20);
+    text('FOLLOW THE SOUND INTO THE BASEMENT [1]',550,550);
+
+    textFont(montserrat);
+    textSize(20);
+    text('GO UPSTAIRS [2]',200,250);
+}
+
+function basementscene1(){ //lights are off
+    background(0);
+    onbasementscene1 = true;
+
+    textFont(montserrat);
+    textSize(20);
+    text('TURN ON THE LIGHTS [1]',80,350);
+
+    textFont(montserrat);
+    textSize(20);
+    text('KEEP THE LIGHTS OFF [2]',850,350);
+}
+
+function basementscene2(){ //decide to turn on the lights
+    image(basement,0,0);
+    onbasementscene2=true;
+
+    textFont(montserrat);
+    textSize(20);
+    text('DO NOTHING [1]',80,350);
+
+    textFont(montserrat);
+    textSize(20);
+    text('SCREAM [2]',900,350);
+}
+
+function basementscene3(){ //decide to do nothing - ENDING 1 (SPIDER DANCE PARTY)
+    background(255); //PLACEHOLDER
+    textFont(montserrat);
+    textSize(40);
+    fill(0);
+    text('[ENDING - SPIDER RAVE]',400,350);
+}
+
+function basementscene4(){ //decide to scream - ENDING 2 (EXTERMINATOR)
+    background(255); //PLACEHOLDER
+    textFont(montserrat);
+    textSize(40);
+    fill(0);
+    text('[ENDING - EXTERMINATOR]',400,350);
+}
+
+function basementscene5(){//decide to leave the lights off
+    background(50); //PLACEHOLDER
+    onbasementscene5=true;
+    textFont(montserrat);
+    textSize(40);
+    fill(0);
+    text('[INSERT RED EYES HERE]',400,350);
+
+    textFont(montserrat);
+    textSize(20);
+    text('RUN AWAY [1]',80,350);
+
+    textFont(montserrat);
+    textSize(20);
+    text('POKE IT! [2]',1100,350);
+}
+
+function basementscene6(){//decide to run away - ENDING 3 (DOG JUMPS ON YOU)
+    background(255);//PLACEHOLDER
+    textFont(montserrat);
+    textSize(40);
+    fill(0);
+    text('[ENDING - DOG JUMPS ON YOU]',400,350);
+
+}
+
+function basementscene7(){ //decide to poke - ENDING 4 (PET THE DOG)
+    background(255);//PLACEHOLDER
+    textFont(montserrat);
+    textSize(40);
+    fill(0);
+    text('[ENDING - PETTING THE DOG]',400,350);
+}
+
+function upstairsscene(){//decide to go upstairs
+    image(upstairsHallway,0,0);
+    onupstairsscene=true;
+
+    textFont(montserrat);
+    textSize(20);
+    text('ENTER ROOM [1]',80,350);
+
+    textFont(montserrat);
+    textSize(20);
+    text('APPROACH COFFIN [2]',900,350);
+}
+
+function roomscene(){//decide to enter the room
+    image(bedroom,0,0);
+    onroomscene=true;
+
+    textFont(montserrat);
+    textSize(20);
+    text('SAY BOO [1]',80,350);
+
+    textFont(montserrat);
+    textSize(20);
+    text('THROW SOMETHING [2]',900,350);
+}
+
+function boo(){//decide to say "boo" - ENDING 5 (SCARE THE GHOST)
+    background(255);//PLACEHOLDER
+    textFont(montserrat);
+    textSize(40);
+    fill(0);
+    text('[ENDING - SCARE THE GHOST]',400,350);
+
+}
+
+function thrown(){//decide to throw something - ENDING 6 (HIT THE GHOST)
+    background(255);//PLACEHOLDER
+    textFont(montserrat);
+    textSize(40);
+    fill(0);
+    text('[ENDING - HIT THE GHOST]',400,350);
+
+}
+
+function coffinscene1(){//decide to approach the coffin
+    background(50); //PLACEHOLDER
+    oncoffinscene1=true;
+    textFont(montserrat);
+    textSize(40);
+    fill(0);
+    text('[INSERT CLOSE-UP OF COFFIN]',300,350);
+
+    textFont(montserrat);
+    textSize(20);
+    text('DO NOTHING [1]',80,350);
+
+    textFont(montserrat);
+    textSize(20);
+    text('OPEN THE COFFIN [2]',1000,350);
+
+}
+
+function coffinscene2(){//decide to do nothing - ENDING 7 (SUNLIGHT)
+    background(255);//PLACEHOLDER
+    textFont(montserrat);
+    textSize(40);
+    fill(0);
+    text('[ENDING - SUNLIGHT + VAMPIRE]',400,350);
+
+}
+
+function coffinscene3(){//decide to open the coffin - ENDING 7 (TOMATO JUICE)
+    background(255);//PLACEHOLDER
+    textFont(montserrat);
+    textSize(40);
+    fill(0);
+    text('[ENDING - TOMATO JUICE + VAMPIRE]',400,350);
+
 }
